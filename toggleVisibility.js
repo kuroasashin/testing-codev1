3476fa1525d7d621072aac8a68ee985823b438eb
Kuro Hanabi <74951321+kuroasashin@users.noreply.github.com>
2026-01-21 09:01:44 +0800

    Rename toggleVisibility function to myFunction

--- toggleVisibility.js
+++ toggleVisibility.js
@@ -1,7 +1,7 @@
-function toggleVisibility() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none" || x.style.display === "") {
-    x.style.display = "block"; // Or "inline", "flex", etc., depending on the element type
+function myFunction() {
+  var x = document.getElementById("myIframe");
+  if (x.style.display === "none") {
+    x.style.display = "block";
   } else {
     x.style.display = "none";
   }
